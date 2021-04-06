f838a4ab7734c2d1066425530b073ce405a662b7
Mike G <moonshine@gmail.com>
2021-04-05 20:29:34 -0700

    Initial endpoint for getting current game
    Get current steam app id from registry, add an endpoint to get it.

---
+++ pages/api/currentgame.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+var regedit = require('regedit')
+
+export default (req, res) => {
+  let results = [];
+  let appId;
+
+  regedit.list('HKCU\\SOFTWARE\\Valve\\Steam')
+  .on('data', function(entry) {
+    //console.log(entry.key)
+    console.log(entry.data.values)
+
+    appId = entry.data.values.RunningAppID.value
+    console.log(appId)
+  })
+  .on('finish', function () {
+    console.log('list operation finished')
+    res.status(200).json({ appid: appId });
+  });
+
+
+}
