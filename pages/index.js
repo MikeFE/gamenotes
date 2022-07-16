9adc66f7609edd54d0cfb67dec5f6185ec4116a8
Mike G <moonshine@gmail.com>
2022-07-15 17:23:11 -0700

    Update material-UI and use flexbox "Grid" component for main layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-import {Container} from '@material-ui/core';
-import Alert from 'react-bootstrap/Alert'
-
-import ProcessList from '../components/processlist'
+import {Grid, Paper} from '@mui/material';
 import Menu from '../components/menu'
 
 // CKEditor 5 uses a bunch of references to 'window' in its code,
 // we need to dynamically load here and tell Next not to use SSR,
 // only run this component in the browser where 'window' is defined.
+// See: https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr
 const NoteEditor = dynamic(() => import('../components/editor'), {ssr: false});
 
 export default function Home() {
@@ -22,17 +20,22 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <Container className="header container-shadow" maxWidth="sm" disableGutters>
-        <div className="titlebar">
-          <i class="fas fa-3x fa-file-alt note-icon" />
-          <h4 className="mb-0 mt-2">Test Game - Example Note 1</h4>
-        </div>
-      </Container>
-      <Container className="content container-shadow" maxWidth="sm" disableGutters>
-        <NoteEditor />
-      </Container>
-      {/*<ProcessList />*/}
+    <div id="root">
+      <Grid container spacing={1} className="grid-container">
+          <Grid item xs={3} className="grid-item-empty"></Grid>
+          <Grid item xs={8} className="grid-item grid-item-shadow">
+            <div className="titlebar">
+              <i className="fas fa-3x fa-file-alt note-icon"/>
+              <h4 className="mb-0 mt-2">Test Game - Example Note 1</h4>
+            </div>
+          </Grid>
+        <Grid item xs={3} className="grid-item grid-item-shadow">
+          <Menu />
+        </Grid>
+        <Grid item xs={8} className="grid-item grid-item-shadow">
+          <NoteEditor />
+        </Grid>
+      </Grid>
     </div>
   )
 }
